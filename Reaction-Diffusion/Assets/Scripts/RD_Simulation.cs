a88f7d860e64170df3a4ce22e3de957000bad35a
matthew-mcc <77706531+matthew-mcc@users.noreply.github.com>
2024-10-25 14:47:55 -0600

    We do be reacting and diffusing

--- Reaction-Diffusion/Assets/Scripts/RD_Simulation.cs
+++ Reaction-Diffusion/Assets/Scripts/RD_Simulation.cs
@@ -9,26 +9,102 @@ public class RD_Simulation : MonoBehaviour
     public int height = 256;
     public ComputeShader computeShader;
 
-    public RenderTexture renderTexture;
+    public float deltaTime = 0.1f;
+    public float feedRate = 0.055f;
+    public float killRate = 0.062f;
+    public float diffusionRateA = 1.0f;
+    public float diffusionRateB = 0.5f;
+
+    private RenderTexture currentGrid;
+    private RenderTexture nextGrid;
+    private RenderTexture displayGrid;
+
+    bool updateDisplay = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        renderTexture = new RenderTexture(width, height, 24);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();      
 
-        computeShader.SetTexture(0, "Result", renderTexture);  
-        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        Time.fixedDeltaTime = 1 / 60.0f;
 
+        
+        Initialize();
         var material = transform.GetComponentInChildren<MeshRenderer>().material;
-        material.mainTexture = renderTexture;
+        material.mainTexture = displayGrid;
 
+    }
+
+    void Initialize(){
+
+        // renderTexture = display
+        displayGrid = new RenderTexture(width, height, 24);
+        displayGrid.enableRandomWrite = true;
+        displayGrid.Create();   
+
+        currentGrid = new RenderTexture(width, height, 24);
+        currentGrid.enableRandomWrite = true;
+        currentGrid.Create();
+
+        nextGrid = new RenderTexture(width, height, 24);
+        nextGrid.enableRandomWrite = true;
+        nextGrid.Create();
+
+        computeShader.SetInt("width", width);
+        computeShader.SetInt("height", height);
+
+        computeShader.SetTexture(0, "displayGrid", displayGrid);  
+        computeShader.SetTexture(0, "currentGrid", currentGrid);
+        computeShader.SetTexture(0, "nextGrid", nextGrid);
+        computeShader.Dispatch(0, width / 8, height / 8, 1);
+
+        UpdateShaderParameters();
+    }
+
+ 
+
+    void UpdateShaderParameters(){
+        computeShader.SetFloat("deltaTime", deltaTime);
+        computeShader.SetFloat("feedRate", feedRate);
+        computeShader.SetFloat("killRate", killRate);
+        computeShader.SetFloat("diffusionRateA", diffusionRateA);
+        computeShader.SetFloat("diffusionRateB", diffusionRateB);
+    }
+    
+    void Simulate(){
+        computeShader.SetFloat("deltaTime", Time.fixedDeltaTime);
+        computeShader.SetTexture(1, "currentGrid", currentGrid);
+        computeShader.SetTexture(1, "nextGrid", nextGrid);
+        computeShader.SetTexture(1, "displayGrid", displayGrid);
+
+        UpdateShaderParameters();
+
+        computeShader.Dispatch(1, width / 8, height / 8, 1);
+
+        Graphics.Blit(nextGrid, currentGrid);
+    }
+
+    void Display(){
+        computeShader.SetTexture(2, "currentGrid", currentGrid);
+        computeShader.SetTexture(2, "displayGrid", displayGrid);
+        computeShader.Dispatch(2, width/8, height/8, 1);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        for (int i = 0; i < 4; i++){
+            Simulate();
+        }
         
+        updateDisplay = true;
+
     }
+
+    private void Update() {
+        if (updateDisplay){
+            Display();
+            updateDisplay = false;
+        }
+    }
+    
 }
